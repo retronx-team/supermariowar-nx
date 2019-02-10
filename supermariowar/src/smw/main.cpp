7b0cd1678ce9b1552b4c100bcff664aefed9557f
p-sam <p-sam@d3vs.net>
2019-02-10 10:49:04 +0000

    File logging when not using nxlink

--- supermariowar/src/smw/main.cpp
+++ supermariowar/src/smw/main.cpp
@@ -58,16 +58,45 @@ extern "C" {
 #include <switch/runtime/devices/socket.h>
 #include <switch/runtime/nxlink.h>
 #include <switch/services/applet.h>
-void userAppInit() {
-    socketInitializeDefault();
-    nxlinkStdio();
+#include <stdio.h>
+#include <unistd.h>
 
-    appletInitializeGamePlayRecording();
+static FILE* g_logfile = NULL;
+void userAppInit() {
+    Result rc = socketInitializeDefault();
+    int stdioRes = -1;
+
+    if(R_SUCCEEDED(rc)) {
+        stdioRes = nxlinkStdio();
+    }
+
+    if(stdioRes != 0) {
+        g_logfile = fopen("smw.log", "w");
+        if(g_logfile != NULL) {
+            fflush(stdout);
+            dup2(fileno(g_logfile), STDOUT_FILENO);
+            fflush(stderr);
+            dup2(fileno(g_logfile), STDERR_FILENO);
+        }
+        printf("nxlink failed with res: %d\n", stdioRes);
+    }
+
+    if(R_FAILED(rc)) {
+        printf("socketInitialize failed with rc: %x\n", rc);
+    }
+
+    rc = appletInitializeGamePlayRecording();
+    if(R_FAILED(rc)) {
+        printf("appletInitializeGamePlayRecording failed with rc: %x\n", rc);
+    }
     appletSetGamePlayRecordingState(1);
 }
 
 void userAppExit() {
     socketExit();
+    if(g_logfile != NULL) {
+        fclose(g_logfile);
+    }
 }
 }
 #endif
