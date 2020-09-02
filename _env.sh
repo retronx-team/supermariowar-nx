7c16eca6ae55a556b3f4262910eec939ad8629bc
p-sam <p-sam@d3vs.net>
2020-09-02 13:35:47 +0000

    data is now a submodule upstream

--- _env.sh
+++ _env.sh
@@ -2,3 +2,6 @@ APP_NAME="smw"
 APP_TITLE="SuperMarioWar"
 APP_AUTHOR="ported by p-sam"
 APP_VERSION="2.0.0-nx3"
+
+SMW_DATA_REPO="mmatyas/supermariowar-data"
+SMW_DATA_VERSION="5ca9ca680a8163574abc10806e62e4885296218f"
--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 set -eo pipefail
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUILD_DIR="$ROOT_DIR/build"
+OUT_DIR="$ROOT_DIR/out"
+
 source "$ROOT_DIR/_env.sh"
 
 if [[ -z "$DEVKITPRO" ]]; then
@@ -14,19 +17,37 @@ if [[ -z "$MAKE_OPTS" ]]; then
 	MAKE_OPTS="-j$(nproc --all)"
 fi
 
+SMW_DATA_ARCHIVE_PATH=""
+if [[ "$SKIP_DATA" != "1" ]]; then
+	mkdir -p "$BUILD_DIR"
+	SMW_DATA_ARCHIVE_PATH="$BUILD_DIR/data_$SMW_DATA_VERSION.tar.gz"
+
+	if [[ ! -f "$SMW_DATA_ARCHIVE_PATH" ]]; then
+		rm -f "$BUILD_DIR/data_tmp.tar.gz" &>/dev/null ||:
+		SMW_DATA_URL="https://github.com/$SMW_DATA_REPO/archive/$SMW_DATA_VERSION.tar.gz"
+		if which curl &> /dev/null; then
+			curl --fail -L "$SMW_DATA_URL" --output "$BUILD_DIR/data_tmp.tar.gz"
+		elif which wget &> /dev/null; then
+			wget "$SMW_DATA_URL" -O "$BUILD_DIR/data_tmp.tar.gz"
+		else
+			echo "No suitable tool to download data found in PATH" 1>&2
+			exit 1
+		fi
+		mv -f "$BUILD_DIR/data_tmp.tar.gz" "$SMW_DATA_ARCHIVE_PATH"
+	fi
+fi
+
 if [[ "$1" == "debug" ]]; then
-	BUILD_DIR="$ROOT_DIR/build/debug"
+	PROGRAM_BUILD_DIR="$ROOT_DIR/build/debug"
 	CMAKE_BUILD_TYPE="Debug"
 	PROGRAM_SUFFIX="-debug"
 else
-	BUILD_DIR="$ROOT_DIR/build/release"
+	PROGRAM_BUILD_DIR="$ROOT_DIR/build/release"
 	CMAKE_BUILD_TYPE="Release"
 	PROGRAM_SUFFIX=""
 fi
 
-OUT_DIR="$ROOT_DIR/out"
-
-mkdir -p "$BUILD_DIR"
+mkdir -p "$PROGRAM_BUILD_DIR"
 
 source "$DEVKITPRO/switchvars.sh"
 
@@ -39,7 +60,7 @@ DEPS_CFLAGS="$("$SWITCH_PKG_CONFIG" $DEPS --cflags)"
 DEPS_LDFLAGS="$("$SWITCH_PKG_CONFIG" $DEPS --libs)"
 DEPS_PLATFORM_LIBS="$(grep -oP -- '-l\K\S*' <<< "$DEPS_LDFLAGS" | paste -sd ";" -)"
 
-cd "$BUILD_DIR"
+cd "$PROGRAM_BUILD_DIR"
 cmake -G"Unix Makefiles" \
 	-DCMAKE_TOOLCHAIN_FILE="$DEVKITPRO/switch.cmake" \
 	-DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
@@ -56,10 +77,13 @@ make $MAKE_OPTS -- smw
 
 mkdir -p "$OUT_DIR"
 
-unzip -o -q "$ROOT_DIR/supermariowar/data.zip" -d "$OUT_DIR"
-echo "Assets copied to $OUT_DIR"
+if [[ -n "$SMW_DATA_ARCHIVE_PATH" ]]; then
+	mkdir -p "$OUT_DIR/data"
+	tar xf "$SMW_DATA_ARCHIVE_PATH" --strip-components=1 --keep-newer-files -C "$OUT_DIR/data" 2>&1 | (grep -q -v "is newer or same age" 1>&2 ||:)
+	echo "Assets copied to $OUT_DIR"
+fi
 
-cp -f "$BUILD_DIR/Binaries/$CMAKE_BUILD_TYPE/smw" "$OUT_DIR/$APP_NAME$PROGRAM_SUFFIX.elf"
+cp -f "$PROGRAM_BUILD_DIR/Binaries/$CMAKE_BUILD_TYPE/smw" "$OUT_DIR/$APP_NAME$PROGRAM_SUFFIX.elf"
 
 "$DEVKITPRO/tools/bin/nacptool" --create "$APP_TITLE$PROGRAM_SUFFIX" "$APP_AUTHOR" "$APP_VERSION" "$OUT_DIR/$APP_NAME$PROGRAM_SUFFIX.nacp"
 "$DEVKITPRO/tools/bin/elf2nro" "$OUT_DIR/$APP_NAME$PROGRAM_SUFFIX.elf" "$OUT_DIR/$APP_NAME$PROGRAM_SUFFIX.nro" \
